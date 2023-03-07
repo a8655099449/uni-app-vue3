4f95165f6bd966271e7b64f0568876e00005e8d3
liuxiang <liuxiang@163.com>
2023-03-07 18:21:45 +0800

    feat:新增目录结构

---
+++ src/config/test/baseConfig.js
@@ -0,0 +1,14 @@
+﻿/*
+ * @Author: liuxiang liuxiang@163.com
+ * @Date: 2022-12-06 16:57:47
+ * @LastEditors: liuxiang liuxiang@163.com
+ * @LastEditTime: 2023-03-07 18:15:40
+ * @FilePath: /uni-app-vue3/src/config/test/baseConfig.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const baseConfig = {
+  version: '1.0.1',
+  baseUrl: 'https://api-test.hajhzh.cn', // 测试环境
+};
+
+module.exports = baseConfig;
